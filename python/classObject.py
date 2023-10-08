d10dab6e906e4c54178dc8c6a06b5bbd85f1eda3
ankitshukla1008 <ankitshuklaji1008@gmail.com>
2023-10-08 05:48:35 +0530

    Adding a ne python folder

---
+++ python/classObject.py
@@ -0,0 +1,12 @@
+#Implementation of class and object in python.
+class Summantion: 
+    def __init__(self,a,b): 
+        self.x=a
+        self.y=b
+    def sum(self):
+        s=self.x + self.y
+        return s #Function returns value
+obj1=Summantion(7,8) #Object creation
+obj2=Summantion(11,12)
+print("Sum of 7 and 8= ",obj1.sum()) #Function calling and print result
+print("Sum of 11 and 12= ",obj2.sum())
