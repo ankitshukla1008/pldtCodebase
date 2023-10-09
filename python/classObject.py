b70ad01b08cb615c8d2237b4e25972f0a1d0ecdb
ankitshukla1008 <ankitshuklaji1008@gmail.com>
2023-10-09 06:17:09 +0530

    adding new features

--- python/classObject.py
+++ python/classObject.py
@@ -6,7 +6,11 @@ class Summantion:
     def sum(self):
         s=self.x + self.y
         return s #Function returns value
+    def product(self):
+        p=self.x * self.y
+        return p #Function returns value
 obj1=Summantion(7,8) #Object creation
 obj2=Summantion(11,12)
 print("Sum of 7 and 8= ",obj1.sum()) #Function calling and print result
 print("Sum of 11 and 12= ",obj2.sum())
+print("Multiplication= ",obj1.product())
